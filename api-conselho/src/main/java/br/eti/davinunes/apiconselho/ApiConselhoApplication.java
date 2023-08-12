27bfe5f010422655d3fc52265c0480d28b079c5b
Davi Nunes <davi.nunes@gmail.com>
2023-08-12 15:15:19 +0000

    add spring project

---
+++ api-conselho/src/main/java/br/eti/davinunes/apiconselho/ApiConselhoApplication.java
@@ -0,0 +1,13 @@
+package br.eti.davinunes.apiconselho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConselhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConselhoApplication.class, args);
+	}
+
+}
---
+++ api-conselho/src/test/java/br/eti/davinunes/apiconselho/ApiConselhoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.eti.davinunes.apiconselho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConselhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
