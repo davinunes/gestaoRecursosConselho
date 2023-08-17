35438bda1780940fd66d39e5f5aeb5581b5dc0c9
Davi Nunes <davi.nunes@gmail.com>
2023-08-17 15:52:46 +0000

    Entrega  PDF e  Base64, mas,  pdf trunca

--- api-conselho/src/main/java/br/eti/davinunes/apiconselho/controller/PdfController.java
+++ api-conselho/src/main/java/br/eti/davinunes/apiconselho/controller/PdfController.java
@@ -3,18 +3,26 @@ package br.eti.davinunes.apiconselho.controller;
 import java.io.ByteArrayOutputStream;
 import java.util.Base64;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
 @Controller
 public class PdfController {
 
-    @PostMapping(value = "/generate-pdf", produces = "application/pdf")
+    @PostMapping(value = "/generate-pdf", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     @ResponseBody
-    public byte[] generatePdf(@RequestBody String htmlContent) {
+        public ResponseEntity<?> generatePdf(
+            @RequestParam String tipo,
+            @RequestBody String htmlContent) {
+
         try {
             ITextRenderer renderer = new ITextRenderer();
             renderer.setDocumentFromString(htmlContent, null);
@@ -23,11 +31,22 @@ public class PdfController {
             renderer.layout();
             renderer.createPDF(outputStream);
 
-            return outputStream.toByteArray();
+            byte[] pdfBytes = outputStream.toByteArray();
+
+            if ("base64".equals(tipo)) {
+                String pdfBase64 = convertToBase64(pdfBytes);
+                return ResponseEntity.ok(pdfBase64);
+            } else if ("file".equals(tipo)) {
+                HttpHeaders headers = new HttpHeaders();
+                headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=converted.pdf");
+                return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
+            } else {
+                return ResponseEntity.badRequest().body("Tipo inválido");
+            }
         } catch (Exception e) {
             e.printStackTrace();
             // Handle exceptions and return an appropriate response
-            return new byte[0];
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro na geração do PDF");
         }
     }
 
