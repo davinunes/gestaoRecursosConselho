7fe7db8a2fb23388f11dfc38d40053d7d516ecec
Davi Nunes <davi.nunes@gmail.com>
2023-09-21 18:51:57 +0000

    Apenas validando o POST

--- api-conselho/src/main/java/br/eti/davinunes/apiconselho/controller/RelatorioController.java
+++ api-conselho/src/main/java/br/eti/davinunes/apiconselho/controller/RelatorioController.java
@@ -1,13 +1,8 @@
 package br.eti.davinunes.apiconselho.controller;
 
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,44 +10,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.eti.davinunes.apiconselho.entity.RelatorioData;
-import jakarta.servlet.http.HttpServletResponse;
-import net.sf.jasperreports.engine.JasperExportManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.JasperRunManager;
 
 @RestController
 @RequestMapping("/relatorio")
 public class RelatorioController {
 
-    @Autowired
-    private JasperReport jasperReport; // Injete o relatório Jasper aqui
-
     @PostMapping("/gerar")
-    public ResponseEntity<ByteArrayResource> gerarRelatorio(@RequestBody RelatorioData data, HttpServletResponse response) {
+    public ResponseEntity<String> gerarRelatorio(@RequestBody RelatorioData data) {
         try {
-            // Crie um mapa com os parâmetros necessários para o relatório
-            Map<String, Object> params = new HashMap<>();
-            params.put("parametro1", data.getParametro1());
-            params.put("listaDeParametros", data.getListaDeParametros());
+            // Acesso aos parâmetros recebidos via POST
+            String parametro1 = data.getParametro1();
+            List<String> listaDeParametros = data.getListaDeParametros();
 
-            // Gere o relatório em Jasper
-            InputStream jasperStream = getClass().getResourceAsStream("/seu_relatorio.jasper"); // Substitua pelo caminho do seu relatório Jasper
-            JasperPrint jasperPrint = JasperRunManager.runReportToPdf(jasperStream, params);
+            // Validando se os dados foram lidos corretamente
+            System.out.println("parametro1: " + parametro1);
+            System.out.println("listaDeParametros: " + listaDeParametros);
 
-            // Converta o relatório em PDF em um array de bytes
-            byte[] pdfBytes = JasperExportManager.exportReportToPdf(jasperPrint);
-
-            // Crie uma resposta HTTP com o PDF
-            ByteArrayResource resource = new ByteArrayResource(pdfBytes);
-            return ResponseEntity.ok()
-                    .header("Content-Disposition", "inline; filename=relatorio.pdf")
-                    .contentType(MediaType.APPLICATION_PDF)
-                    .body(resource);
+            // Retornando uma resposta de sucesso
+            return ResponseEntity.ok("Dados lidos com sucesso: " + parametro1 + ", " + listaDeParametros);
         } catch (Exception e) {
+            // Tratamento de erros
             e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao processar os dados.");
         }
     }
 }
-
