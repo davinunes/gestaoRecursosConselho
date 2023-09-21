9394f11e9d600166f2f7db72d71ddf78d866ca83
Davi Nunes <davi.nunes@gmail.com>
2023-09-21 22:31:17 +0000

    Quase funcionando, tem algum erro no relatorio

--- api-conselho/src/main/java/br/eti/davinunes/apiconselho/controller/RelatorioController.java
+++ api-conselho/src/main/java/br/eti/davinunes/apiconselho/controller/RelatorioController.java
@@ -1,8 +1,13 @@
 package br.eti.davinunes.apiconselho.controller;
 
+import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,31 +16,62 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.eti.davinunes.apiconselho.entity.DetalhesTabela;
 import br.eti.davinunes.apiconselho.entity.RelatorioData;
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @RestController
 @RequestMapping("/relatorio")
 public class RelatorioController {
 
+    
+
     @PostMapping("/gerar")
-    public ResponseEntity<String> gerarRelatorio(@RequestBody RelatorioData data) {
+    public ResponseEntity<byte[]> gerarRelatorio(@RequestBody RelatorioData data) {
+
         try {
-            // Acesso aos parâmetros recebidos via POST
-            String parametro1 = data.getParametro1();
-            List<DetalhesTabela> listaDeParametros = data.getListaDeParametros();
+            InputStream jasperStream = getClass().getResourceAsStream("/br/eti/davinunes/apiconselho/relatorios/Coffee.jasper");
 
-            // Validando se os dados foram lidos corretamente
-            System.out.println("parametro1: " + parametro1);
-            
-            for (DetalhesTabela detalhes : listaDeParametros) {
-                System.out.println("DetalhesTabela: coluna1=" + detalhes.getColuna1() + ", coluna2=" + detalhes.getColuna2() + ", coluna3=" + detalhes.getColuna3() + ", coluna4=" + detalhes.getColuna4());
-            }
+            // Crie um mapa para os parâmetros do relatório
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("parametro1", data.getParametro1());
 
-            // Retornando uma resposta de sucesso
-            return ResponseEntity.ok("Dados lidos com sucesso: " + parametro1 + ", " + listaDeParametros);
+            // Converta a lista de DetalhesTabela em um JRDataSource (dependendo da sua lógica)
+            JRDataSource dataSource = createDataSource(data.getListaDeParametros());
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperStream, parameters, dataSource);
+
+            byte[] relatorioPDF = JasperExportManager.exportReportToPdf(jasperPrint);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_PDF);
+            headers.setContentDispositionFormData("attachment", "relatorio.pdf");
+
+            return ResponseEntity.ok()
+            .headers(headers)
+            .contentLength(relatorioPDF.length)
+            .body(relatorioPDF);
         } catch (Exception e) {
-            // Tratamento de erros
             e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao processar os dados.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new byte[0]);
+        }
+
+
+        
+    }
+
+    // Método para criar o JRDataSource a partir da lista de DetalhesTabela
+    private JRDataSource createDataSource(List<DetalhesTabela> detalhesTabelaList) {
+        // Verifique se a lista não é nula ou vazia
+        if (detalhesTabelaList != null && !detalhesTabelaList.isEmpty()) {
+            // Use JRBeanCollectionDataSource para mapear a lista para um JRDataSource
+            return new JRBeanCollectionDataSource(detalhesTabelaList);
+        } else {
+            // Se a lista estiver vazia, use um JRDataSource vazio
+            return new JREmptyDataSource();
         }
     }
 }
